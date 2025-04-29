5159824f5ca7a03cdd69c2329a6b2d8ae90e6bd6
cocoman043 <rocampita@up.edu.ph>
2025-04-29 10:25:34 +0800

    feat: i3lock-color

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,5 +1,43 @@
 #!/bin/sh
 set -e
 xset s off dpms 0 10 0
-i3lock -t -i ~/dotfiles/wallpaper/black_hole.png --ignore-empty-password --show-failed-attempts --nofork
+# i3lock -t -i ~/dotfiles/wallpaper/black_hole.png --ignore-empty-password --show-failed-attempts --nofork
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#ff00ffcc'
+TEXT='#ee00eeee'
+WRONG='#880000bb'
+VERIFYING='#bb00bbbb'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 5                     \
+-i ~/dotfiles/wallpaper/black_hole.png \
+-t \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"       \
+--keylayout 1                \
+--nofork
+
 xset s off -dpms
