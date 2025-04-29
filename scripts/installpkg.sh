f7f4a71877afed356ae59475a42f796c7b9859fe
cocoman043 <rocampita@up.edu.ph>
2025-04-29 10:36:40 +0800

    feat: declaratiev package management

--- scripts/installpkg.sh
+++ scripts/installpkg.sh
@@ -1,2 +1,8 @@
-yay -Syu --needed - < ~/dotfiles/pkglist.txt && \
-yay -Qqe > ~/dotfiles/pkglist.txt
+
+if [ $# -gt 0 ]; then
+    command yay "$@"
+else
+    command yay -Syu --needed - < ~/dotfiles/pkglist.txt 
+fi
+
+command yay -Qqe > ~/dotfiles/pkglist.txt
