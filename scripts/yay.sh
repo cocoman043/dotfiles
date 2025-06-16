126cb2dbbb0547af311cadff6981b9293efa937c
cocoman043 <rocampita@up.edu.ph>
2025-06-16 08:24:39 +0800

    feat: added colors to yay

--- scripts/yay.sh
+++ scripts/yay.sh
@@ -1,1 +1,1 @@
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && sudo cp ../pacman.conf /etc/pacman.conf
